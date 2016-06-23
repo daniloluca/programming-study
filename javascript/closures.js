6d212bcffa8d8212963aea4c301c59f9d60311e6
daniloluca <danilolucasmd@gmail.com>
2016-06-22 21:52:37 -0300

    Adicionado arquivo de estudo sobre closures no javascript.

---
+++ javascript/closures.js
@@ -0,0 +1,26 @@
+//Closures
+
+// significado = fechamento, encerramento, encarceramento
+
+// Exemplo:
+
+var helloWorld = function(){
+	var message = 'Hello World!';
+	return function(){
+		return message;
+	};
+};
+
+console.log(helloWorld); //[Function]
+console.log(helloWorld()); //[Function]
+console.log(helloWorld()()); //Hello World!
+
+// Apesar da função retornada por helloWorld() ser uma inner function que se retornada e executada externamente
+// logicamente não teria acesso a variavel message, ela mantem as referencias externas que ela faz,
+// tornando assim possivel que a variavel message seja acessada mesmo não estando no escopo da função.
+
+// As Closures são muito importantes, pois permite a utilização de callbacks.
+// Os callbacks mantes os recursos externos acessados por eles para executar quando forem chamados, criando closures.
+
+// São as Closures que nos permitem, por exemplo, passar uma função por parametro e continuar acessando os recursos
+// externos a ela quando ela for executada em outro escopo.
