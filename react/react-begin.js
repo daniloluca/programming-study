4a3add8f86083b812e03b2a85e1ae8186407c6fa
daniloluca <danilolucasmd@gmail.com>
2016-07-05 22:24:30 -0300

    Atualizados alguns arquivos e estrutura de diretorios.

---
+++ react/react-begin.js
@@ -0,0 +1,5 @@
+// Nesta obra estaremos abordando o React, uma biblioteca criada pela equipe do
+// Facebook que tem como principal propósito implementar a camada de visualização
+// de uma aplicação web através da criação de componentes, beneficiando-se de um
+// uso muito bem elaborado da DOM, o que garante uma boa performance em relação
+// às demais bibliotecas
