36a37ddb78d741f02c400521537a86e75574900d
daniloluca <danilolucasmd@gmail.com>
2017-01-30 10:23:32 -0200

    Aula 03.05

--- courses/Alura/JavaScript avançado I ES6, orientação a objetos e padrões de projetos/js/app/controllers/NegociacaoController.js
+++ courses/Alura/JavaScript avançado I ES6, orientação a objetos e padrões de projetos/js/app/controllers/NegociacaoController.js
@@ -10,8 +10,7 @@ class NegociacaoController {
 	adiciona(event){
 		event.preventDefault();
 
-		console.log(typeof this._inputData.value);
-
-		console.log(this._inputData.value);
+		let data = new Date(this._inputData.value.replace(/-/g, ','));
+		console.log(data);
 	}
 }
