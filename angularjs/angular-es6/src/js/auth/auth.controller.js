e0f17166921792df7a7c066849190499df210647
daniloluca <danilolucasmd@gmail.com>
2017-04-24 14:03:37 +0100

    Restricting access to pages for logged in/out users

--- angularjs/angular-es6/src/js/auth/auth.controller.js
+++ angularjs/angular-es6/src/js/auth/auth.controller.js
@@ -3,18 +3,21 @@ class AuthCtrl {
 	    'ngInject';
 
 	    this.title = $state.current.title;
-	    this.authType = $state.current.name.replace('app.', '');
 	    this._User = User;
+	    
+	    this.authType = $state.current.name.replace('app.', '');
+	    this._$state = $state;
 	}
 
 	submitForm() {
 		this.isSubmitting = true;
 
 		this._User.attemptAuth(this.authType, this.formData).then(
+			// Callback for success
 			(res) => {
-				this.isSubmitting = false;
-				console.log(res);
+				this._$state.go('app.home');
 			},
+			// Callback for failure
 			(err) => {
 				this.isSubmitting = false;
 				this.errors = err.data.errors;
